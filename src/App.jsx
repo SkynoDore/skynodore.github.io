9447cb5bbb5abe123635b6d4ce2b5aa09d0b167e
Gabriel <gabriel-vich02@hotmail.com>
2025-05-21 00:38:24 +0200

    moving to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid px-5">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="views/galeria.html">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="views/contacto.html">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="views/contacto.html">https://github.com/SkynoDore</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+        <main class="text-light bg-dark">
+            <section class="p-5">
+                <div class="container p-5 pt-1 d-block d-none d-md-block">
+                <div class="d-flex flex-row">
+                    <div class="d-flex flex-column justify-content-center gap-3 m-3">
+                        <h1>Hello, I'm <span class="text-light-emphasis">Gabriel Vich</span></h1>
+                        <h2>Front End Developer</h2>
+                    </div>
+                
+                </div>
+                </div>
+            </section>
+
+            <section class="p-3">
+                <h3 class="text-center pb-3">About me</h3>
+                <p>
+                    <span class="text-light-emphasis">Full-Stack Web Developer </span>with two years of experience in web design and application development using Laravel and React. I am looking for a Junior Web Developer position to continue growing professionally and applying my knowledge.
+                    I have worked with agile methodologies such as Scrum. I have strong problem-solving skills, learn quickly, and enjoy acquiring new knowledge, so I am constantly improving my skills.
+
+                </p>
+            </section>
+
+            <section class="p-3">
+            <h3 class="text-center">Skills</h3>
+            <div class="d-flex flex-row p-5 justify-content-center gap-3">
+            <i class="devicon-html5-plain-wordmark"></i>
+            <i class="devicon-css3-plain-wordmark "></i>
+            <i class="devicon-java-plain"></i>
+            <i class="devicon-javascript-plain"></i>
+            <i class="devicon-php-plain"></i>
+            <i class="devicon-react-original"></i>
+            <i class="devicon-angular-plain"></i>
+            <i class="devicon-azuresqldatabase-plain"></i>
+            <i class="devicon-git-plain"></i>
+            </div>
+            </section>
+
+
+            <section class="p-3">
+            <h3 class="text-center">Projects overview</h3>
+            <div class="d-flex flex-row p-5 justify-content-center gap-3">
+                <div class="bg-primary p-3 rounded-2 d-flex flex-column gap-3">
+                    <h4>Página web en wordpress</h4>
+
+                </div>
+                 <div class="bg-primary p-3 rounded-2 d-flex flex-column gap-3">
+                    <h4>Página web en wordpress</h4>
+
+                </div>
+            </div>
+            </section>
+        </main>
     </>
   )
 }
