58e3f38e5cd14eb534ae1eafbfd0644844e8838d
Gabriel <gabriel-vich02@hotmail.com>
2025-05-22 15:19:47 +0200

    yess it's looking good

--- dev/src/App.jsx
+++ dev/src/App.jsx
@@ -1,86 +1,64 @@
-
+import Header from './components/header';
+import Project from './components/project';
 
 function App() {
 
   return (
     <>
-    <header>
-        <nav class="navbar mb-0 navbar-expand-lg navbar-dark bg-primary">
-            <div class="container-fluid px-5">
-               
-                    <ul class="navbar-nav me-auto mb-lg-0 d-flex flex-row w-100 justify-content-between">
-                        <div class="d-flex w-50 gap-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.html">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="views/galeria.html">Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="views/contacto.html">Contact</a>
-                        </li>
-                        </div>
-                        <div class="d-flex">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://github.com/SkynoDore">Github</a>
-                        </li>
-                        </div>
-                    </ul>
-         
-            </div>
-        </nav>
-    </header>
-        <main class="text-light bg-dark">
-            <section class="p-5">
-                <div class="container p-5 pt-1 s">
-                <div class="d-flex flex-row">
-                    <div class="d-flex flex-column justify-content-center gap-3 m-3">
-                        <h1>Hello, I'm <span class="text-light-emphasis">Gabriel Vich</span></h1>
+    <Header/>
+        <main className="text-light bg-dark">
+            <section className="p-4 p-md-5">
+                <div className="pt-1">
+                <div className="d-flex flex-row justify-content-around">
+                    <div className="d-flex flex-column justify-content-center gap-3">
+                        <h1>Hello, I'm <span className="text-light-emphasis">Gabriel Vich</span></h1>
                         <h2>Front End Developer</h2>
                     </div>
-                <img class="rounded-circle object-fit-cover" src="/images/gabriel-main.jpeg" alt="Gabriel Vich Portrait" />
+                <img id="portrait" className="rounded-circle object-fit-cover" src="/images/gabriel-main.jpeg" alt="Gabriel Vich Portrait" />
 
                 </div>
                 </div>
             </section>
 
-            <section class="p-3">
-                <h3 class="text-center pb-3">About me</h3>
+            <section className="p-4 p-md-5 w-100 w-md-50">
+                <h3 className="text-center pb-3">About me</h3>
                 <p>
-                    <span class="text-light-emphasis">Full-Stack Web Developer </span>with two years of experience in web design and application development using Laravel and React. I am looking for a Junior Web Developer position to continue growing professionally and applying my knowledge.
+                    <span className="text-light-emphasis">Full-Stack Web Developer </span>with two years of experience in web design and application development using Laravel and React. I am looking for a Junior Web Developer position to continue growing professionally and applying my knowledge.
                     I have worked with agile methodologies such as Scrum. I have strong problem-solving skills, learn quickly, and enjoy acquiring new knowledge, so I am constantly improving my skills.
 
                 </p>
             </section>
 
-            <section class="p-3">
-            <h3 class="text-center">Skills</h3>
-            <div class="d-flex flex-row p-5 justify-content-center gap-3">
-            <i class="devicon-html5-plain-wordmark"></i>
-            <i class="devicon-css3-plain-wordmark "></i>
-            <i class="devicon-java-plain"></i>
-            <i class="devicon-javascript-plain"></i>
-            <i class="devicon-php-plain"></i>
-            <i class="devicon-react-original"></i>
-            <i class="devicon-angular-plain"></i>
-            <i class="devicon-azuresqldatabase-plain"></i>
-            <i class="devicon-git-plain"></i>
+            <section className="p-4 p-md-5 w-100 w-md-75">
+            <h3 className="text-center pb-3">Skills</h3>
+            <div className="d-flex justify-content-center gap-3 flex-wrap">
+                <i className="devicon-html5-plain-wordmark" title="HTML5"></i>
+                <i className="devicon-css3-plain-wordmark " title="CS3"></i>
+                <i className="devicon-javascript-plain" title="Javascript"></i>
+                <i className="devicon-react-original" title="React.js"></i>
+                <i className="devicon-angular-plain" title="Angular"></i>
+                <i className="devicon-laravel-original" title="Laravel"></i>
+                <i className="devicon-git-plain" title="Git"></i>
+                <i className="devicon-nodejs-plain-wordmark" title="Node.js"></i>
+                <i className="devicon-nextjs-original-wordmark" title="Next.js"></i>
+                <i className="devicon-azuresqldatabase-plain" title="SQL"></i>
+                <i className="devicon-bootstrap-plain" title="Bootstrap"></i>
+                <i className="devicon-tailwindcss-original" title="Tailwind"></i>
+                <i className="devicon-php-plain" title="PHP"></i>
+                <i className="devicon-java-plain" title="JAVA"></i>
+                <i className="devicon-figma-plain" title="Figma"></i>
             </div>
             </section>
 
-
-            <section class="p-3">
-            <h3 class="text-center">Projects overview</h3>
-            <div class="d-flex flex-row p-5 justify-content-center gap-3">
-                <div class="bg-primary p-3 rounded-2 d-flex flex-column gap-3">
-                    <h4>Página web en wordpress</h4>
-
+            <section className="p-4 p-md-5 w-100 w-md-75">
+            <h3 className="text-center pb-3">Projects overview</h3>
+                <div className="projects d-flex flex-row justify-content-center gap-3">
+                        <Project title="Red social en Laravel" image="images/diseño1.jpeg"/>
+                        <Project title="Portafolio En React" image="images/diseño1.jpeg"/>
+                        <Project title="Página en Wordpress" image="images/diseño1.jpeg"/>
+                        
+                    
                 </div>
-                 <div class="bg-primary p-3 rounded-2 d-flex flex-column gap-3">
-                    <h4>Página web en wordpress</h4>
-
-                </div>
-            </div>
             </section>
         </main>
     </>
---
+++ dev/src/components/header.jsx
@@ -0,0 +1,30 @@
+export default function Header() {
+    return (
+        <header>
+        <nav className="navbar mb-0 navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid px-5">
+               
+                    <ul className="navbar-nav me-auto mb-lg-0 d-flex flex-row w-100 justify-content-between">
+                        <div className="d-flex w-50 gap-3">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="index.html">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="views/galeria.html">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="views/contacto.html">Contact</a>
+                        </li>
+                        </div>
+                        <div className="d-flex">
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://github.com/SkynoDore">Github</a>
+                        </li>
+                        </div>
+                    </ul>
+         
+            </div>
+        </nav>
+    </header>
+    );
+}
