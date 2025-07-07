01195ce6a77c71f2b3d401eeae9df7c06d1183db
Gabriel <40807192+SkynoDore@users.noreply.github.com>
2025-07-07 21:16:40 +0200

    card design change

--- dev/src/components/project.jsx
+++ dev/src/components/project.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Project ({ title, image, link}){
     return (
         <Link className="nav-link" to={link}>
-                <div className="bg-primary p-3 rounded-2 gap-3 h-100">
+                <div className="bg-primary p-3 rounded-2 gap-3 h-100 d-flex flex-column align-items-center justify-content-center">
                     <h4 className="text-break mb-3"  style={{ width: "300px" }}>{title}</h4>
                     <div className="image-container">
                         <img src={image} alt={title} />
