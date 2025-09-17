65ce58c5624edaa25675482ab505e34a2ac25e45
Gabriel <40807192+SkynoDore@users.noreply.github.com>
2025-09-17 13:58:54 +0200

    minimun style change

--- dev/src/components/footer.jsx
+++ dev/src/components/footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <header>
         <nav className="bg-primary">
-            <div className="container-fluid p-4 p-md-5 pb-4">
+            <div className="container-fluid p-4 p-md-5 pb-md-4">
                         <div className="d-flex flex-column align-items-center">
                             <p>© 2025 <a href="https://www.linkedin.com/in/slytrox/" target="_blank">Gabriel Vich.</a> All rights reserved.</p>
                             <p>This website was built by myself with React and Bootstrap. :D</p>
