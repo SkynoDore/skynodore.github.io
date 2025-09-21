fb5ca63976eda2a9c04153e919d514a024b5e99f
Gabriel <40807192+SkynoDore@users.noreply.github.com>
2025-09-22 00:43:53 +0200

    add zoom modal on expanded project

--- dev/src/components/expanded-project.jsx
+++ dev/src/components/expanded-project.jsx
@@ -12,6 +12,9 @@ export default function ExpandedProject({
 
   // miniaturas: todas las demás imágenes excepto la principal actual
   const thumbnails = images;
+
+  //para el zoom
+  const [zoomedImg, setZoomedImg] = useState(null);
   return (
     
     <main>
@@ -21,7 +24,8 @@ export default function ExpandedProject({
           <h1>{title}</h1>
         </div>
         <div className="d-flex flex-column flex-md-row  justify-content-center gap-0 gap-md-3 mt-2 mb-2 mb-md-5 max-width-900 mx-auto">
-          <img id="main-image" src={mainImage} alt={title} />
+          <img id="main-image" src={mainImage} alt={title} 
+          onClick={() => setZoomedImg(mainImage)}/>
 
           {/* Miniaturas en lateral o debajo, aquí simplificado en horizontal */}
           <div className="d-flex flex-row flex-md-column flex-wrap gap-3 align-items-center justify-content-md-start justify-content-center">
@@ -31,7 +35,7 @@ export default function ExpandedProject({
                 src={img}
                 alt={`${title} thumbnail ${idx}`}
                 onMouseEnter={() => setMainImage(img)}
-                className="thumbnail"
+                className={`thumbnail ${img === mainImage ? "active" : ""}`}
               />
             ))}
           </div>
@@ -48,6 +52,26 @@ export default function ExpandedProject({
           <h2 className="pb-3">Description</h2>
           <div dangerouslySetInnerHTML={{ __html: description }} />
         </div>
+        {zoomedImg && (
+        <div
+          className="modal show d-block"
+          tabIndex="-1"
+          onClick={() => setZoomedImg(null)}
+        >
+          <div className="modal-dialog modal-dialog-centered modal-xl">
+            <div className="modal-content bg-primary p-0 p-md-3 rounded-3 w-100" >
+              <div className="modal-body text-center w-100 p-0 p-md-1">
+                <img src={zoomedImg} alt="zoomed" className="img-fluid w-100 rounded" id="zoomed-img"/>
+              </div>
+              <button
+                type="button"
+                className="btn-close btn-close-white position-absolute top-0 end-0 p-1"
+                aria-label="Close"
+                onClick={() => setZoomedImg(null)}
+              ></button>
+            </div>
+          </div>
+        </div>)}
       </section>
     </main>
   );
